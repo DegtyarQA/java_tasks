9c2c41d8aceb2ded0ec48ab15d840fdebc64d954
vdegtjar <v.degtjar@cft.ru>
2023-12-04 17:12:11 +0300

    day 1 is done

---
+++ src/main/java/day1/Task6.java
@@ -0,0 +1,15 @@
+package day1;
+
+public class Task6 {
+
+    public static void main(String[] args) {
+        int k = 7;
+        int mnozitel = 1;
+        while (mnozitel<10){
+            int proizved = k * mnozitel;
+            System.out.println(mnozitel + " " + "x" + " " + k + " "+ "=" +" " + proizved);
+            mnozitel++;
+        }
+
+    }
+}
