ee6c518d27e9539322fdcbaf1f90d40405e725b6
vdegtjar <v.degtjar@cft.ru>
2023-12-04 17:18:25 +0300

    fix style

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -5,11 +5,10 @@ public class Task6 {
     public static void main(String[] args) {
         int k = 7;
         int mnozitel = 1;
-        while (mnozitel<10){
+        while (mnozitel < 10) {
             int proizved = k * mnozitel;
-            System.out.println(mnozitel + " " + "x" + " " + k + " "+ "=" +" " + proizved);
+            System.out.println(mnozitel + " " + "x" + " " + k + " " + "=" + " " + proizved);
             mnozitel++;
         }
-
     }
 }
